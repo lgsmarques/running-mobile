38ab4f11683f6a2a48c96977ba3d391337a9263c
lgsmarques <luizgsmarques@gmail.com>
2023-06-12 17:47:24 -0300

    creating level manager

---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform container;
+    public List<GameObject> levels;
+
+    private int _index;
+    private GameObject _currentLevel;
+
+    private void Awake()
+    {
+        SpawnNextLevel();
+    }
+
+    private void SpawnNextLevel()
+    {
+        if(_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+
+            if(_index >= levels.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currentLevel = Instantiate(levels[_index], container);
+        _currentLevel.transform.localPosition = Vector3.zero;
+    }
+
+    private void ResetLevelIndex()
+    {
+        _index = 0;
+    }
+}
