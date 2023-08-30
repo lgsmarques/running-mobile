8e3091f3e7c7f7b6e9c570db81d15c1ec1751b55
lgsmarques <luizgsmarques@gmail.com>
2023-08-30 19:42:58 -0300

    add random level generator

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -7,12 +7,22 @@ public class LevelManager : MonoBehaviour
     public Transform container;
     public List<GameObject> levels;
 
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public List<LevelPieceBase> levelPieces;
+    public int piecesStartNumber = 3;
+    public int piecesEndNumber = 1;
+    public int piecesNumber = 5;
+
     private int _index;
     private GameObject _currentLevel;
+    private List<LevelPieceBase> _spawnedPieces;
 
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -36,4 +46,50 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new();
+
+        for (int i = 0; i < piecesStartNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }
+
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+
+        for (int i = 0; i < piecesEndNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> pieces)
+    {
+        var piece = pieces[Random.Range(0, pieces.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    //IEnumerator CreateLevelPiecesCoroutine()
+    //{
+    //    _spawnedPieces = new();
+
+    //    for (int i = 0; i < piecesNumber; i++)
+    //    {
+    //        CreateLevelPiece();
+    //        yield return new WaitForSeconds(timeBeteweenPieces);
+    //    }
+    //}
 }
